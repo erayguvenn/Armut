2869f7d17769a9f8368b6cf7f32b161ac18f4748
Eray Güven <eray0736@gmail.com>
2022-10-09 19:16:16 +0300

    bug fix

--- src/app/module/home-page/home-page.module.ts
+++ src/app/module/home-page/home-page.module.ts
@@ -3,12 +3,10 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
 import {HomePageRoutingModule} from "./home-page-routing.module";
-<<<<<<< HEAD
 import { CardComponent } from './components/card/card.component';
-=======
 import { NavbarInputButtonComponent } from './components/navbar-input-button/navbar-input-button.component';
 import { FormSearchComponent } from './components/form-search/form-search.component';
->>>>>>> 234cc559b8fd1f341da5274c8cae8fe3c4f6a093
+
 
 
 
@@ -16,12 +14,10 @@ import { FormSearchComponent } from './components/form-search/form-search.compon
   declarations: [
     NavbarComponent,
     HomeComponent,
-<<<<<<< HEAD
-    CardComponent
-=======
+    CardComponent,
     NavbarInputButtonComponent,
     FormSearchComponent
->>>>>>> 234cc559b8fd1f341da5274c8cae8fe3c4f6a093
+
   ],
   exports: [
     NavbarComponent,
