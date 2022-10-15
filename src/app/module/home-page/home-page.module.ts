800d4658b824fccc72e99e2e1e048267343b8021
Eray Güven <eray0736@gmail.com>
2022-10-15 13:15:01 +0300

    add cards component

--- src/app/module/home-page/home-page.module.ts
+++ src/app/module/home-page/home-page.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
 import {HomePageRoutingModule} from "./home-page-routing.module";
-import { CardComponent } from './components/card/card.component';
 import { NavbarInputButtonComponent } from './components/navbar-input-button/navbar-input-button.component';
 import { FormSearchComponent } from './components/form-search/form-search.component';
+import { CardsComponent } from './components/cards/cards.component';
 
 
 
@@ -14,9 +14,9 @@ import { FormSearchComponent } from './components/form-search/form-search.compon
   declarations: [
     NavbarComponent,
     HomeComponent,
-    CardComponent,
     NavbarInputButtonComponent,
-    FormSearchComponent
+    FormSearchComponent,
+    CardsComponent
 
   ],
   exports: [
