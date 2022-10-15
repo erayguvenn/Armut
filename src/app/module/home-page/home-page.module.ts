a257b4bfee4c2535fb26b28948b17525c7676ee6
Eray Güven <eray0736@gmail.com>
2022-10-15 14:58:30 +0300

    home page design

--- src/app/module/home-page/home-page.module.ts
+++ src/app/module/home-page/home-page.module.ts
@@ -6,6 +6,7 @@ import {HomePageRoutingModule} from "./home-page-routing.module";
 import { NavbarInputButtonComponent } from './components/navbar-input-button/navbar-input-button.component';
 import { FormSearchComponent } from './components/form-search/form-search.component';
 import { CardsComponent } from './components/cards/cards.component';
+import { HizmetlerLinkComponent } from './components/hizmetler-link/hizmetler-link.component';
 
 
 
@@ -16,7 +17,8 @@ import { CardsComponent } from './components/cards/cards.component';
     HomeComponent,
     NavbarInputButtonComponent,
     FormSearchComponent,
-    CardsComponent
+    CardsComponent,
+    HizmetlerLinkComponent
 
   ],
   exports: [
