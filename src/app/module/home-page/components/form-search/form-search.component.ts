b4c79768805ab972c0dcc7ea68c9dd427d7c81f8
Eray Güven <eray0736@gmail.com>
2022-12-02 02:03:37 +0300

    hizmetim edit

--- src/app/module/home-page/components/form-search/form-search.component.ts
+++ src/app/module/home-page/components/form-search/form-search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ActivatedRoute, Router} from '@angular/router';
+import { pickBy, identity,isEmpty } from 'lodash-es';
 @Component({
   selector: 'app-form-search',
   templateUrl: './form-search.component.html',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormSearchComponent implements OnInit {
 
-  constructor() { }
+  params={}
+  constructor(private router: Router,
+              private activatedRoute: ActivatedRoute) { }
+
 
   ngOnInit(): void {
+   this.params={
+        hizmet:"",
+        sayfa:""
+
+    }
+  }
+
+  getSearch(searchInput:string){
+    let mainPage="hizmet"
+    if (searchInput=="giris"||searchInput=="giriş"||searchInput=="giris yap"||searchInput=="giriş yap"){
+      mainPage="giris"
+    }
+    else if (searchInput=="üye"||searchInput=="üye ol"||searchInput=="uye"||searchInput=="uye ol"){
+      mainPage="giris"
+    }
+    else if (searchInput=="yardim"||searchInput=="yardım"){
+      mainPage="yardim"
+    }
+    else if (searchInput=="hizmet ver"||searchInput=="hızmet ver"){
+      mainPage="hizmetver"
+    }
+
+    this.params={
+      sayfa:searchInput
+    }
+      /*this.router.navigateByUrl('/'+searchInput);*/
+    this.router.navigate(["/"+mainPage], {
+      relativeTo: this.activatedRoute,
+      queryParams: pickBy(this.params, identity)
+
+    });
   }
 
 }
--- src/app/module/home-page/pages/hizmet/hizmet.component.ts
+++ src/app/module/home-page/pages/hizmet/hizmet.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {WorkCategoryService} from "../../services/work-category.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {filter, first} from "rxjs";
+import { pickBy, identity,isEmpty } from 'lodash-es';
+
 
 @Component({
   selector: 'app-hizmet',
@@ -10,22 +14,108 @@ export class HizmetComponent implements OnInit {
 
   workCategoryList:any[]=[]
   p: number = 1;
+  params={}
+  category:any=[]
+  categoryName:any=[]
+  hizmetTitle:string="Temizlik Hizmetleri"
 
-  constructor(private workCategoryService:WorkCategoryService) { }
+  constructor(private router: Router,
+              private activatedRoute: ActivatedRoute,
+              private workCategoryService:WorkCategoryService) {
+
+    this.params={
+      page:  '',
+
+    }
+
+  }
 
 
   ngOnInit(): void {
     this.workCategory()
+    this.getCategoryName()
   }
 
   workCategory(){
-
     return this.workCategoryService.getWorkCategory().subscribe(
       data=>{
         this.workCategoryList.push(data)
         console.log(this.workCategoryList)
+
+        for (let catg of this.workCategoryList[0]){
+          if (catg.parentId==this.getPageId()){
+            this.category.push(catg)
+          }
+        }
+        this.getCategoryName()
       } ,
       err => console.log("Category verileri getirilemedi"))
+
   }
+
+  getCategoryName(){
+    for (const key in this.category) {
+      if (this.category.hasOwnProperty(key)) {
+        length += 1;
+        this.categoryName.push(this.category[key].name)
+      }
+    }
+
+  }
+
+  getPageId(){
+    let sayfa=this.activatedRoute.snapshot?.queryParams?.["sayfa"];
+    let parentId=0;
+
+    switch (sayfa){
+      case "temizlik":{
+        parentId=1
+        this.hizmetTitle="Temizlik Hizmetleri"
+        break;
+      }
+      case "tadilat":{
+        parentId=2
+        this.hizmetTitle="Tadilat Hizmetleri"
+
+        break;
+      }
+      case "nakliyat":{
+        parentId=3
+        this.hizmetTitle="Nakliyat Hizmetleri"
+
+        break;
+      }
+      case "tamir":{
+        parentId=4
+        this.hizmetTitle="Tamir Hizmetleri"
+
+        break;
+      }
+      case "özel ders":{
+        parentId=5
+        this.hizmetTitle="Özel Ders Hizmetleri"
+
+        break;
+      }
+      case "sağlık":{
+        parentId=6
+        this.hizmetTitle="Sağlık Hizmetleri"
+
+        break;
+      }
+      case "düğün":{
+        parentId=7
+        this.hizmetTitle="Düğün Hizmetleri"
+
+        break;
+      }
+
+
+    }
+    return parentId
+  }
+
+
+
 }
 
