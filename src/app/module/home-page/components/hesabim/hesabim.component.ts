d52e72f39b4f0c06a9c66ccfd9df0b0c535055b5
omer7 <omer.70942@gmail.com>
2022-12-15 14:15:38 +0300

    personal page

---
+++ src/app/module/home-page/components/hesabim/hesabim.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hesabim',
+  templateUrl: './hesabim.component.html',
+  styleUrls: ['./hesabim.component.scss']
+})
+export class HesabimComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
